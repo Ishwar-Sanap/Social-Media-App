ea4a5bee99689f02abdbbd1862763dc530c640d8
Ishwar-Sanap <ishwarsanap512@gmail.com>
2026-03-07 23:49:02 +0530

    changes to fix CORS issue

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,13 +2,17 @@ import express from "express";
 import "dotenv/config";
 import connectDB from "./configs/db.js";
 import cookieParser from "cookie-parser";
+import cors from 'cors'
 import userAuthRouter from "./routes/userAuthRoutes.js";
 import userProfileRouter from "./routes/userProfileRoutes.js";
 import connectionRouter from "./routes/connectionRoutes.js";
 import postRouter from "./routes/postRoutes.js";
 import storyRouter from "./routes/storyRoutes.js";
 const app = express();
-
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials:true
+}))
 await connectDB();
 
 app.use(express.json()); //middleware to parse JSON data
